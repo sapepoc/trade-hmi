75a6b75edf626f4254cdc067ae777ded8c5fd097
SAPIENT\nrohil <nrohilla@sapient.com>
2017-09-20 18:15:40 +0530

    removing wrongly committed project in the original repo

--- ruleengine-service/src/main/java/com/sap/config/TestBean.java
+++
@@ -1,8 +0,0 @@
-package com.sap.config;
-
-public class TestBean {
-	public String testMethod(String msg) {
-		return "Heo1 ____________________ " + msg;
-	}
-
-}
--- ruleengine-service/src/main/java/com/sap/config/TestBean3.java
+++
@@ -1,7 +0,0 @@
-package com.sap.config;
-
-public class TestBean3 {
-	public String testMethod(String msg) {
-		return "Heo3 ____________________ " + msg;
-	}
-}
